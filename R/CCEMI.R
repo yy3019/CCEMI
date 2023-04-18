0c5dc0ede6ae20b61049938f4d82271a1462fca3
yy3019 <yy3019@cumc.columbia.edu>
2023-04-18 15:23:35 -0500

    CCEMI

--- R/CCEMI.R
+++ R/CCEMI.R
@@ -1,8 +1,8 @@
-#' Constrained Iterative Conditional Imputation (CICI)
+#' Constrained Chained Equations Multiple Imputation (CCEMI)
 #'
 #' Multiple Imputation of More than One Exposure with Non-differential Measurement Error
 #'
-#' Constrained Iterative Conditional Imputation (CICI) treats the true concentrations in the new samples as missing and utilize multiple imputation to fill in these values based on the non-differential measurement error (NDME) assumption.
+#' Constrained Chained Equations Multiple Imputation (CCEMI) treats the true concentrations in the new samples as missing and utilize multiple imputation to fill in these values based on the non-differential measurement error (NDME) assumption.
 #'
 #' @param data A data frame or a matrix containing the incomplete data. Missing values are coded as NA.
 #' @param predM A numeric matrix of length(blocks) rows and ncol(data) columns, containing 0/1 data specifying the set of predictors to be used for each target column. Each row corresponds to a variable block, i.e., a set of variables to be imputed. A value of 1 means that the column variable is used as a predictor for the target block (in the rows). The matrix should base on the calibration design you want.
@@ -14,11 +14,11 @@
 #' @param seed An integer that is used as argument by the set.seed() for offsetting the random number generator. Default is to leave the random number generator alone.
 #' @return impst: A list of imputed datasets. imp_complete: Complete imputed data. result: Point estimate, ci, variance
 #' @author Yuanzhi Yu, Qixuan Chen
-#' @references \url{https://github.com/yy3019/CICI}
+#' @references \url{https://github.com/yy3019/CCEMI}
 #' @importFrom bootImpute mice AER MCMCglmm
 #' @export
-#' @examples see \url{https://github.com/yy3019/CICI}
-#' @name CICI
+#' @examples see \url{https://github.com/yy3019/CCEMI}
+#' @name CCEMI
 #'
 #'
 
